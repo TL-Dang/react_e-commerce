fc4b7969526e110613fdf85c33dff20f1051cefb
Tommy Dang <ledangt310@live.com>
2023-03-23 17:09:27 -0700

    create category-item component, add scss

--- README.md
+++
@@ -1,1 +0,0 @@
-# react_e-commerce
---
+++ web-store/src/App.js
@@ -0,0 +1,42 @@
+import CategoryItem from './components/category-item/category-item.component';
+import './categories.style.scss';
+
+function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Shoes',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'Women',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Men',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
+      ))}
+    </div>
+  );
+}
+
+export default App;
