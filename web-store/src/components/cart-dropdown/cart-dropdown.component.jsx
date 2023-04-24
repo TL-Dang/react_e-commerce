18283a40899c0a3aaf0c56eb55cd9f5448e5b319
Tommy Dang <ledangt310@live.com>
2023-04-24 16:40:12 -0700

    fix checkout route, add cart-item and checkout-item components

--- web-store/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ web-store/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
@@ -6,17 +8,21 @@ import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = function () {
-  const {cartItems} = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
         {cartItems.map((item) => (
-          <CartItem key={item.div} cartItem={item} />
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-
-      <Button>Go To Checkout</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
--- web-store/src/contexts/cart.context.jsx
+++ web-store/src/contexts/cart.context.jsx
@@ -19,11 +19,31 @@ const addCartItem = function (cartItems, productToAdd) {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  //find item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  //check if quantity is equal to 1, if it is remove item from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  //return back cartitems w/ matching cart item with reduced quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
   cartCount: 0,
 });
 
@@ -47,10 +67,15 @@ export const CartProvider = function ({ children }) {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
+  const removeItemToCart = function (cartItemToRemove) {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemToCart,
     cartItems,
     cartCount,
   };
